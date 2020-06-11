ad775c02f667a5a2d82a3ae376b886104337f7d7
Thomas Maghanga <maghangamail@gmail.com>
2020-06-11 21:05:15 +0300

    [Create] note display activity layout file

---
+++ app/src/main/java/com/example/mynotesapp/ui/NoteActivity.java
@@ -0,0 +1,18 @@
+package com.example.mynotesapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynotesapp.R;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+
+    }
+}
