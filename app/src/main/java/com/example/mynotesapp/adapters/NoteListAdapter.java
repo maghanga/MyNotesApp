11519dd2a64508030841a91be04328c3f4a3e773
Thomas Maghanga <maghangamail@gmail.com>
2020-06-15 22:16:16 +0300

    [Add] view holder to the note list adapter

--- app/src/main/java/com/example/mynotesapp/adapters/NoteListAdapter.java
+++ app/src/main/java/com/example/mynotesapp/adapters/NoteListAdapter.java
@@ -1,21 +1,35 @@
 package com.example.mynotesapp.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NoteListAdapter extends RecyclerView.Adapter {
+import com.example.mynotesapp.R;
+
+public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteListViewHolder> {
+    private final Context context;
+    private final LayoutInflater layoutInflater;
+
+    public NoteListAdapter(Context context) {
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NoteListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = layoutInflater.inflate(R.layout.activity_note_list, parent, false);
+        return new NoteListViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NoteListViewHolder holder, int position) {
 
     }
 
@@ -24,10 +38,17 @@ public class NoteListAdapter extends RecyclerView.Adapter {
         return 0;
     }
 
-    public class NoteListViewHolder extends RecyclerView.ViewHolder {
+    public static class NoteListViewHolder extends RecyclerView.ViewHolder {
+        public TextView noteTitleTextView;
+        public TextView categoryTitleTextView;
 
         public NoteListViewHolder(@NonNull View itemView) {
             super(itemView);
+            noteTitleTextView = (TextView) itemView.findViewById(R.id.noteTitleText);
+            categoryTitleTextView = (TextView) itemView.findViewById(R.id.categoryTitleText);
+
         }
+
+
     }
 }
